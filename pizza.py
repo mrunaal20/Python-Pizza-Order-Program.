1cef191329955c721337374b68d967eccbf957ae
Mrunaal Chincholikar <61539555+mrunaal20@users.noreply.github.com>
2022-07-15 14:25:55 +0530

    This fixes #1
    It will calculate the bill for more pizza.

--- pizza.py
+++ pizza.py
@@ -1,28 +1,30 @@
 # 🚨 Don't change the code below 👇
 print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M, or L ")
-add_pepperoni = input("Do you want pepperoni? Y or N ")
-extra_cheese = input("Do you want extra cheese? Y or N ")
-# 🚨 Don't change the code above 👆
-
-#Write your code below this line 👇
+Num_of_pizza = int(input("Enter the number of pizza you want to order?"))
 bill = 0
-
-if size == "S":
-  bill += 15
-elif size == "M":
-  bill += 20
-else:
-  bill += 25
-
-if add_pepperoni == "Y":
-  if size == "S":
-    bill += 2
-  else:
-    bill += 3
+for i in range(Num_of_pizza):
+    size = input("What size pizza do you want? S, M, or L ")
+    add_pepperoni = input("Do you want pepperoni? Y or N ")
+    extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
+#Write your code below this line 👇
+    if size == "S":
+      bill += 100
+    elif size == "M":
+      bill += 150
+    else:
+      bill += 200
     
-if extra_cheese == "Y":
-  bill += 1
-  
-print(f"Your final bill is: ${bill}.")
-
+    if add_pepperoni == "Y":
+      if size == "S":
+        bill +=20
+      else:
+        bill +=30
+        
+    if extra_cheese == "Y":
+      bill += 10
+    Num_of_pizza += 1 
+    
+    print(f"Your bill is: Rs{bill}.")
+    print('-' * 50)
+print(f"Your final bill is: Rs{bill}.")
